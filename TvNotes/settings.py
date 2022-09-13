e5f7c6004a3c3675ea489432565b556880478cb6
unknown <kamrozzaman.azad@gmail.com>
2022-09-13 19:40:16 +0600

    SDA

--- TvNotes/settings.py
+++ TvNotes/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 LOGIN_URL = 'login_page'
 LOGOUT_REDIRECT_URL = 'home_page'
-django_heroku.settings(locals())
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
